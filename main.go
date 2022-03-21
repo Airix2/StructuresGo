70e78e9096138ae312f1674abb77a97fe16e5b59
Airix2 <alex.soto@cetys.edu.mx>
2022-03-21 16:26:46 -0700

    finish

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/Airix2/StructuresGo/pkg/customer"
+	"github.com/Airix2/StructuresGo/pkg/invoice"
+	"github.com/Airix2/StructuresGo/pkg/invoiceitem"
+)
+
+func main() {
+	i := invoice.New(
+		"Colombia",
+		"Paris",
+		customer.New("Alejandro", "Zona Rio", "665123423"),
+		[]invoiceitem.Item{
+			invoiceitem.New(1, "Curso Go", 12.5),
+			invoiceitem.New(2, "Poo Go", 15.5),
+			invoiceitem.New(3, "DB con Go", 20.0),
+		},
+	)
+	i.SetTotal()
+	fmt.Printf("%+v", i)
 }
--- pkg/invoice/invoice.go
+++ pkg/invoice/invoice.go
@@ -1,10 +1,31 @@
 package invoice
 
-import "../pkg/customer"
+import (
+	"github.com/Airix2/StructuresGo/pkg/customer"
+	"github.com/Airix2/StructuresGo/pkg/invoiceitem"
+)
 
+// Inovice is the struct of a invoice
 type Invoice struct {
 	country string
 	city    string
 	total   float64
 	client  customer.Customer
+	items   []invoiceitem.Item
+}
+
+func New(country, city string, client customer.Customer, items []invoiceitem.Item) Invoice {
+	return Invoice{
+		country: country,
+		city:    city,
+		client:  client,
+		items:   items,
+	}
+}
+
+// Set Total is the Setter of Invoice.total
+func (i *Invoice) SetTotal() {
+	for _, item := range i.items {
+		i.total += item.Value()
+	}
 }
